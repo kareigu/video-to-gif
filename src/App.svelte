899a10bd75fa2af0f5a1bf34ab94fbf690a63d5a
mxr <eetu.kivelae@gmail.com>
2021-01-17 17:46:40 +0200

    Added loading and converting a full video file

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,46 @@
 <script lang='ts'>
-	import {onMount} from 'svelte';
-	let count: number = 0;
-	let stopCount = false;
+	import { onMount } from 'svelte';
+	import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+	const VIDEO = 'video.mp4'
+	const GIF = 'out.gif'
+
+	const ffmpeg = createFFmpeg({ log: true });
+	let videoFile: File | null;
+	let ffmpegReady = false;
+	let gifFile: string = '';
+
+	async function loadFFMPEG() {
+		await ffmpeg.load();
+		ffmpegReady = true;
+	}
 
 	onMount(() => {
-	  const interval = setInterval(() => {
-			if(stopCount)
-				clearInterval(interval);
-			count++;
-		}, 1000);
-	  return () => {
-			clearInterval(interval);
-	  };
+		loadFFMPEG();
 	});
+
+	function handleLoadingVideo(e: Event) {
+		if(e.target) {
+			const target: HTMLInputElement = e.target;
+			videoFile = target.files?.item(0) ? target.files?.item(0) : null;
+		}
+	}
+
+	async function convertToGif() {
+		if(videoFile) {
+			ffmpeg.FS('writeFile', VIDEO, await fetchFile(videoFile));
+			await ffmpeg.run('-i', VIDEO, GIF);
+
+			const data = ffmpeg.FS('readFile', GIF);
+			gifFile = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+			ffmpeg.FS('unlink', VIDEO)
+		}
+	}
+
+	async function clearGif() {
+		ffmpeg.FS('unlink', GIF);
+		gifFile = '';
+	}
 
 </script>
   
@@ -21,58 +49,34 @@
 	margin: 0;
 	font-family: Arial, Helvetica, sans-serif;
 }
+
 .App {
 	text-align: center;
-}
-.App code {
-	background: #0002;
-	padding: 4px 8px;
-	border-radius: 4px;
-}
-.App p {
-	margin: 0.4rem;
-}
-
-.App-header {
-	background-color: #f9f6f6;
-	color: #333;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	font-size: calc(10px + 2vmin);
-}
-.App-link {
-	color: #ff3e00;
-}
-.App-logo {
-	height: 36vmin;
-	pointer-events: none;
-	margin-bottom: 3rem;
-	animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-}
-@keyframes App-logo-spin {
-	from {
-	transform: scale(1);
-	}
-	to {
-	transform: scale(1.06);
-	}
 }
 </style>
   
 <div class="App">
-<header class="App-header">
-	<img src="/logo.svg" class="App-logo" alt="logo" />
-	<p>Edit <code>src/App.svelte</code> and save to reload.</p>
-	<p>Page has been open for <code>{count}</code> seconds.</p>
-	<button on:click={() => stopCount = true}>Stop counter</button>
-	<p>
-	<a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-		Learn Svelte
-	</a>
-	</p>
-</header>
+	<h2>
+		Convert video to GIF
+	</h2>
+	{#if ffmpegReady}
+		<input 
+			type="file" 
+			name="video" 
+			accept="video/*" 
+			on:change={handleLoadingVideo}
+		>
+
+		{#if videoFile}
+			<video src={URL.createObjectURL(videoFile)} controls={true} />
+		{/if}
+
+		<button on:click={convertToGif}>Convert to GIF</button>
+		<button on:click={clearGif}>Clear GIF</button>
+
+		{#if gifFile !== ''}
+			<img src={gifFile} alt="converted gif" />
+		{/if}
+	{/if}
 </div>
   
