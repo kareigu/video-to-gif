ac44b3ca6eca1425c364953ed53a9356c19e2336
mxr <eetu.kivelae@gmail.com>
2021-01-18 00:11:47 +0200

    Added actix-web base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+static PORT: u16 = 8080;
+static WORKERS: usize = 4;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .workers(WORKERS)
+    .bind(format!("127.0.0.1:{}", PORT))?
+    .run()
+    .await
 }
