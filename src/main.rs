1508e5a9de1e70702f29395ff95c21a4e8c49346
mxr <eetu.kivelae@gmail.com>
2021-01-18 00:51:51 +0200

    Can now serve files and uses TLS

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Result, HttpRequest};
+use actix_files::NamedFile;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 static PORT: u16 = 8080;
+static SHUTDOWN_TIME: u64 = 15;
 static WORKERS: usize = 4;
 
-#[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index(_req: HttpRequest) -> Result<NamedFile> {
+    Ok(NamedFile::open("front/build/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+
+    builder.set_private_key_file("certs/key.pem", SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file("certs/cert.pem").unwrap();
+
     HttpServer::new(|| {
         App::new()
-            .service(index)
+            .route("/", web::get().to(index))
+            .service(actix_files::Files::new("/", "front/build"))
     })
+    .shutdown_timeout(SHUTDOWN_TIME)
     .workers(WORKERS)
-    .bind(format!("127.0.0.1:{}", PORT))?
+    .bind_openssl(format!("127.0.0.1:{}", PORT), builder)?
     .run()
     .await
 }
