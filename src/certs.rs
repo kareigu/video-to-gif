6d7fea9a4df94cdaab0c80ca0b58d46e274d32e9
mxr <eetu.kivelae@gmail.com>
2021-01-18 17:58:45 +0200

    Missing cert-files now gives better feedback

--- src/certs.rs
+++ src/certs.rs
@@ -2,8 +2,8 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod, SslAcceptorBuilder};
 
 pub fn read_certs() -> SslAcceptorBuilder {
   let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-  builder.set_private_key_file("certs/key.pem", SslFiletype::PEM).unwrap();
-  builder.set_certificate_chain_file("certs/cert.pem").unwrap();
+  builder.set_private_key_file("certs/key.pem", SslFiletype::PEM).expect("No private key file found");
+  builder.set_certificate_chain_file("certs/cert.pem").expect("No certificate found");
   
   builder
 }
